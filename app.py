3b7d1b91af1a34c6783a506fd0cdddbfd3dbecd5
Muskan <muskanchaurasia2512@gmail.com>
2026-04-20 14:49:30 +0530

    MlDeploy2

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template, url_for
-import pickle
-
+from flask import Flask, request, jsonify, render_template
+import joblib
 
 app = Flask(__name__)
-model = pickle.load(open('randomForestRegressor.pkl','rb'))
 
+model = joblib.load("randomForestRegressor.pkl")
 
 @app.route('/')
 def home():
-    #return 'Hello World'
     return render_template('home.html')
-    #return render_template('index.html')
 
-@app.route('/predict',methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    print(prediction[0])
+    features = [float(x) for x in request.form.values()]
+    final_features = np.array([features])
+    
+    prediction = model.predict(final_features)[0]
+    
+    return render_template(
+        'home.html',
+        prediction_text=f"AQI for Jaipur: {prediction:.2f}"
+    )
 
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
-
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    '''
-    For direct API calls trought request
-    '''
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    prediction = model.predict([np.array(list(data.values()))])[0]
+    return jsonify({'aqi': prediction})
 
-    output = prediction[0]
-    return jsonify(output)
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
